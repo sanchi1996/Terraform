e3a0c62a221596cc0765d0e0e5870991fa2f7f05
sanchi1996 <136577907+sanchi1996@users.noreply.github.com>
2023-09-02 13:32:15 +0530

    Update main.tf

--- ecr/main.tf
+++ ecr/main.tf
@@ -1,1 +1,12 @@
+resource "aws_ecr_repository" "aws_ecr" {
+  name                 = var.repositoryName
+  image_tag_mutability = var.image_tag_mutability
 
+  image_scanning_configuration {
+    scan_on_push = var.scan_on_push
+  }
+  tags = {
+    Name      = var.repositoryName
+    component = var.component
+    }
+}
