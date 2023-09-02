cdafec89921542644ee5e537746e99c81fcaa2ea
sanchi1996 <136577907+sanchi1996@users.noreply.github.com>
2023-09-02 13:33:01 +0530

    Create variables.tf

---
+++ ecr/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    type = string
+}
+variable "repositoryName" {
+    type = string
+}
+variable "scan_on_push" {
+    type = bool
+}
+variable "image_tag_mutability" {
+    type = string
+}
+variable "component" {
+    type = string
+}
